5062b86fa34a2c22954b1b9ee47cd33cacac62f3
Mehmet Özkan <mehmet61ozkan@outlook.com>
2021-12-13 23:54:11 +0300

    .

---
+++ state_management_provider/lib/state_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StateData with ChangeNotifier {
+  String sehir = 'Adana';
+  void newCity(String city) {
+    sehir = city;
+    notifyListeners();
+  }
+}
